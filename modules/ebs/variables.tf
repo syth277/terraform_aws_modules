c840b8fcf0c16b8ab11605be1d93303d2b0410fd
Tahir Hussain <95578284+syth277@users.noreply.github.com>
2024-03-10 22:26:04 +0500

    Create variables.tf

---
+++ modules/ebs/variables.tf
@@ -0,0 +1,47 @@
+variable "application_name" {
+  type    = string
+}
+
+variable "application_discription" {
+  type    = string
+}
+
+variable "environment_name" {
+  type    = string
+}
+
+variable "solution_stack_name" {
+  type    = string
+}
+
+variable "vpc" {
+  type    = string
+}
+
+variable "instance_subnets" {
+  type    = string
+}
+
+variable "instance_type" {
+  type    = string
+}
+
+variable "key_pair" {
+  type    = string
+}
+
+variable "iaminstanceprofile" {
+  type    = string
+}
+
+variable "tier_name" {
+  type    = string 
+}
+
+variable "elbsubnet1" {
+  type    = string
+}
+
+variable "elbsubnet2" {
+  type    = string
+}
