495b9f2b315b259ceb53a9dbaebd25584753b782
Tahir Hussain <95578284+syth277@users.noreply.github.com>
2024-03-11 16:04:21 +0500

    Update ebs.tf

--- ebs.tf
+++ ebs.tf
@@ -2,37 +2,19 @@ provider "aws" {
   region = "us-east-1"
 }
 
-module "ebs" {
-  source = "./modules/ebs"
-
-  application_name        = "Test-App-2"
-  application_discription = "This application is created via terraform"
-  environment_name        = "Test-Env-2"
-  solution_stack_name     = "64bit Amazon Linux 2023 v6.1.1 running Node.js 20"
-  vpc                     = "vpc-02da4ca3dec9e49b6"
-  instance_subnets        = "subnet-094177f911e35a0f5"
-  instance_type           = "t3.micro"
-  key_pair                = "my-keypair"
-  iaminstanceprofile      = "ec2-role-for-elasticbeanstalk"
-  tier_name               = "WebServer"
-  elbsubnet1              = "subnet-099db029060d0182a"
-  elbsubnet2              = "subnet-0e1ae9bc223da2474"
-}
-
 module "ebs_2" {
   source = "./modules/ebs"
 
-  application_name        = "Test-App-3"
-  application_discription = "This application is created via terraform"
-  environment_name        = "Test-Env-3"
-  solution_stack_name     = "64bit Amazon Linux 2023 v6.1.1 running Node.js 20"
-  vpc                     = "vpc-02da4ca3dec9e49b6"
-  instance_subnets        = "subnet-094177f911e35a0f5"
-  instance_type           = "t3.micro"
-  key_pair                = "my-keypair"
-  iaminstanceprofile      = "ec2-role-for-elasticbeanstalk"
-  tier_name               = "WebServer"
-  elbsubnet1              = "subnet-099db029060d0182a"
-  elbsubnet2              = "subnet-0e1ae9bc223da2474"
-
+  application_name    = "Test-App"
+  environment_name    = "Test-Env-3"
+  solution_stack_name = "64bit Amazon Linux 2023 v6.1.1 running Node.js 20"
+  vpc                 = "vpc-028ca6670535b88be"
+  instance_subnet     = "subnet-0999834ab9fe4248f, subnet-006616b3c60da6cd8"
+  instance_type       = "t3.micro"
+  key_pair            = "my-keypair"
+  iaminstanceprofile  = "ec2-role-for-elasticbeanstalk"
+  tier_name           = "WebServer"
+  elbsubnet           = "subnet-0c46a1afd963e8946, subnet-0cdf033e4d1b14386"
+  proxy_server        = "apache"
+  loadbalancer_type   = "application"
 }
